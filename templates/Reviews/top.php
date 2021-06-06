a9e763b0214c93dd63d3b1f37004e686f348d75a
poptype <masat1662@gmail.com>
2021-06-07 00:34:37 +0900

    update

---
+++ templates/Reviews/top.php
@@ -0,0 +1,8 @@
+<?php
+// top.cssの適用
+$this->assign('css', $this->Html->css(['normalize.min', 'milligram.min', 'cake', 'top']));
+?>
+<div class="top-gredContainer">
+<!-- Hero and Guidline  -->
+<section class="hero-wrapper"><p>HERO and GUIDELINE</p></section>
+</div>
