67a5fbe3d69d32e927698c8c267f6fbd7f1ca8fa
poptype <masat1662@gmail.com>
2021-06-08 00:46:43 +0900

    update

--- templates/Reviews/top.php
+++ templates/Reviews/top.php
@@ -3,6 +3,13 @@
 $this->assign('css', $this->Html->css(['normalize.min', 'milligram.min', 'cake', 'top']));
 ?>
 <div class="top-gredContainer">
-<!-- Hero and Guidline  -->
-<section class="hero-wrapper"><p>HERO and GUIDELINE</p></section>
+    <!-- Hero and Guidline  -->
+    <section class="hero-wrapper">
+        <p>HERO and GUIDELINE</p>
+    </section>
+
+    <div class="sort-wrapper">
+        <?= $this->Paginator->sort('created', '日付順') ?>
+        <?= $this->Paginator->sort('voting', '投稿順') ?>
+    </div>
 </div>
