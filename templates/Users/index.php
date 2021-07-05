2daea3a65224c97c8fc4ed7ea852f2545764f50e
poptype <masat1662@gmail.com>
2021-07-06 02:06:30 +0900

    update

--- templates/Users/index.php
+++ templates/Users/index.php
@@ -1,52 +1,73 @@
 <?php
+
 /**
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\User[]|\Cake\Collection\CollectionInterface $users
  */
+// user.cssの適用
+$this->assign('css', $this->Html->css(['normalize.min', 'milligram.min', 'cake', 'user_list']));
+$this->Breadcrumbs->add([
+	['title' => 'Home', 'url' => '/'],
+	['title' => 'ユーザーリスト', 'url' => null]
+]);
+
 ?>
-<div class="users index content">
-    <?= $this->Html->link(__('新規ユーザー登録'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3><?= __('ユーザーリスト') ?></h3>
-    <div class="table-responsive">
-        <table>
-            <thead>
-                <tr>
-                    <th><?= $this->Paginator->sort('アカウント名') ?></th>
-                    <th><?= $this->Paginator->sort('性別') ?></th>
-                    <th><?= $this->Paginator->sort('年齢') ?></th>
-                    <th><?= $this->Paginator->sort('登録日') ?></th>
-                    <th><?= $this->Paginator->sort('編集日') ?></th>
-                    <th><?= $this->Paginator->sort('病名') ?></th>
-                    <th><?= $this->Paginator->sort('email') ?></th>
-                    <th class="actions"><?= __('') ?></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($users as $user) : ?>
-                <tr>
-                    <td>
-                        <?= $this->Html->link($user->username, ['action' => 'view', $user->id]) ?>
-</td>
-                    <td><?= h($user->gender) ?></td>
-                    <td><?= $this->Number->format($user->age) ?></td>
-                    <td><?= h($user->created) ?></td>
-                    <td><?= h($user->modified) ?></td>
-                    <td><?= $user->has('disease_category') ? $this->Html->link($user->disease_category->name, ['controller' => 'DiseaseCategories', 'action' => 'view', $user->disease_category->id]) : '' ?></td>
-                    <td><?= h($user->email) ?></td>
-                    </td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    </div>
-    <div class="paginator">
-        <ul class="pagination">
-            <?= $this->Paginator->first('<< ' . __('first')) ?>
-            <?= $this->Paginator->prev('< ' . __('previous')) ?>
-            <?= $this->Paginator->numbers() ?>
-            <?= $this->Paginator->next(__('next') . ' >') ?>
-            <?= $this->Paginator->last(__('last') . ' >>') ?>
-        </ul>
-        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
-    </div>
+<?= $this->Breadcrumbs->render(
+	['class' => 'breadcrumbs'],
+	['separator' => '>']
+) ?>
+
+<div class="column-responsive column-80">
+	<div class="users index content">
+		<h3 class="heading"><?= __('ユーザーリスト') ?></h3>
+		<div class="sort-wrapper">
+			<?= $this->Paginator->sort('username', 'アカウント名') ?>
+			<?= $this->Paginator->sort('gender', '性別') ?>
+			<?= $this->Paginator->sort('age','年齢') ?>
+			<?= $this->Paginator->sort('created', '登録日') ?>
+			<!-- <!?= $this->Paginator->sort('編集日') ?> -->
+			<?= $this->Paginator->sort('disease_categorie_id', '病名') ?>
+			<!-- <!?= $this->Paginator->sort('email') ?> -->
+		</div>
+
+		<div class="grid_container">
+			<?php foreach ($users as $user) : ?>
+				<div class="user_list">
+					<?php
+					$avatar = $user->avatar;
+					echo $this->Html->image("upload/${avatar}", ['alt' => 'clinic image', 'class' => 'avatar']);
+					?>
+					<?= $this->Html->link(
+						$user->username . "さん",
+						['action' => 'view', $user->id],
+						['class' => 'username']
+					) ?>
+					<span class="gender"><?= h($user->gender) ?>性</span>
+					<span class="age"><?= $this->Number->format($user->age) ?>歳</span>
+					<span class="label">登録日</span>
+					<span class="created"><?= h($user->created->format('Y年m月d日')) ?></span>
+					<span class="label">病名</span>
+					<?= $user->has('disease_category') ? $this->Html->link(
+						$user->disease_category->name,
+						['controller' => 'DiseaseCategories', 'action' => 'view', $user->disease_category->id],
+						['class' => 'disease']
+					) : '' ?>
+					<!-- <!?= h($user->email) ?> -->
+				</div>
+			<?php endforeach; ?>
+		</div>
+
+
+	</div>
+	<div class="paginator">
+		<ul class="pagination">
+			<?= $this->Paginator->first('<< ') ?>
+			<?= $this->Paginator->prev(__('前へ')) ?>
+			<?= $this->Paginator->numbers(['modulus' => 4, 'after' => '…']) ?>
+			<?= $this->Paginator->next(__('次へ')) ?>
+			<?= $this->Paginator->last(' >>') ?>
+			<li class="page_count"><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}')) ?></li>
+		</ul>
+		<!--<p><!?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>-->
+	</div>
 </div>
